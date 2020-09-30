04837806d58446b46853c7ca52aed73aa7f57d91
Evan <evvn@outlook.com>
2020-09-30 21:10:47 +1000

    parsed favorite color to main page for current user

--- app/src/main/java/com/example/assignment_2/InventoryActivity.java
+++ app/src/main/java/com/example/assignment_2/InventoryActivity.java
@@ -3,6 +3,7 @@ package com.example.assignment_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,11 @@ public class InventoryActivity extends AppCompatActivity {
         // inventory screen heading - personalise to current user
         final TextView textHeading = findViewById(R.id.textInventoryHeading);
         String customHeading = "Hi, " + MainActivity.currentUser.getUsername();
+        // set message
         textHeading.setText(customHeading);
+        // set color
+        int favoriteColor = Color.parseColor(MainActivity.currentUser.getFavoriteColor());
+        textHeading.setTextColor(favoriteColor);
 
         // logout button listener
         final ImageView iconLogOut = findViewById(R.id.iconLogOut);
